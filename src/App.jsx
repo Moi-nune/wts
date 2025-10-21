6578b478c91097030d599a031ab5eda2d752727c
Moi-nune <105731475+Moi-nune@users.noreply.github.com>
2025-10-21 12:40:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="page">
+      <div className="letter-container">
+        <div className="letter-paper">
+          <div className="letter-header">
+            <h1>Words to Say</h1>
+            <div className="divider"></div>
+          </div>
+          
+          <div className="letter-content">
+            <p className="verse">I still remember those first few weeks</p>
+            
+            <p className="verse">
+              how I'd unlock my phone<br />
+              just to see if your name was there<br />
+              How my heart would skip a beat whenever it was.
+            </p>
+            
+            <p className="verse">
+              I remember how photos of you made me smile like an idiot.<br />
+              How I'd replay your voice in my head after we spoke.<br />
+              How even just knowing I'd get to hear you later even if just on a call made the whole day feel electric.
+            </p>
+            
+            <p className="verse">
+              It felt like<br />
+              something new.<br />
+              something rare
+            </p>
+            
+            <p className="verse">
+              And the funny thing is,<br />
+              I still feel it.
+            </p>
+            
+            <p className="verse">
+              Because those feelings never really went away.<br />
+              They just settled in and became love.
+            </p>
+            
+            <p className="verse">
+              I still skip a beat when I unlock my phone to see if you texted and when you had.<br />
+              I get a moment of panic when I realise you texted and I missed responding.<br />
+              I still fawn over when I see you send me a photo, especialllllly when unprompted.<br />
+              I still die to hear your voice even if I cannot see you, just on a call, I do.
+            </p>
+            
+            <p className="verse">
+              I apologise, I make things hard<br />
+              I apologise, I made you feel as though, you were not enough, because you are<br />
+              you do not trouble me nor do I have issues with each and every thing you do, I do not<br />
+              and I'm so troubled by the fact I cannot call you and tell this to you in my voice<br />
+              because a text is not enough<br />
+              I cannot give it to you in writing but maybe telling you these this way might be more compelling.
+            </p>
+            
+            <p className="verse">
+              I do not apologise just to apologise,<br />
+              I mean that I did not mean to do that
+            </p>
+            
+            <p className="verse">
+              I hope you understand that you are very important to me and you will never not be.<br />
+              Just you being there is what I wish at the moment and that will always be true no matter what else changes.
+            </p>
+          </div>
+          
+          <div className="letter-footer">
+            <div className="divider"></div>
+            <p className="signature">With all my heart</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
